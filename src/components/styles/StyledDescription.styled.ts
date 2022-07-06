8fccbeeca5dfc47a31484ddeddcfa5c3c434ec70
Wadood <wadood.hussaini@gmail.com>
2022-07-05 21:39:14 -0400

    modify trailer modal styles

--- src/components/styles/StyledDescription.styled.ts
+++ src/components/styles/StyledDescription.styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { device } from "./_breakpoints";
 
 const StyledDescription = styled.div`
-  font-size: 24px;
+  font-size: 20px;
   line-height: 1.25;
   margin-top: 20px;
 
--- src/components/styles/StyledTrailerTitle.ts
+++ src/components/styles/StyledTrailerTitle.ts
@@ -2,11 +2,11 @@ import styled from "styled-components";
 import { device } from "./_breakpoints";
 
 const StyledTrailerTitle = styled.h1`
-  font-size: 60px;
+  font-size: 50px;
   max-width: 85%;
   font-weight: 900;
   margin: 0;
-  margin-bottom: 25px;
+  margin-bottom: 10px;
 
   @media ${device.tablet} {
     display: none;
