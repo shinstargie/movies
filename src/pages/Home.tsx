d20697eec33c794b36e7a5d0a8b8e39df2a132fe
Wadood <wadood.hussaini@gmail.com>
2022-07-14 23:08:06 -0400

    reduce genres on home and set random featured movie

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,12 +13,6 @@ const Home: React.FC = () => {
   const [animation, setAnimation] = useState<Movie[] | null>(null);
   const [comedy, setComedy] = useState<Movie[] | null>(null);
   const [crime, setCrime] = useState<Movie[] | null>(null);
-  const [docu, setDocu] = useState<Movie[] | null>(null);
-  const [drama, setDrama] = useState<Movie[] | null>(null);
-  const [family, setFamily] = useState<Movie[] | null>(null);
-  const [fantasy, setFantasy] = useState<Movie[] | null>(null);
-  const [history, setHistory] = useState<Movie[] | null>(null);
-  const [horror, setHorror] = useState<Movie[] | null>(null);
 
   const [loading, setLoading] = useState<boolean>(true);
   const loadingItems = Array.from({ length: 20 }, (_, i) => i + 1);
@@ -30,20 +24,13 @@ const Home: React.FC = () => {
     getAnimation();
     getComedy();
     getCrime();
-    getDocu();
-    getDrama();
-    getFamily();
-    getFantasy();
-    getHistory();
-    getHorror();
   }, []);
 
   function chooseFeaturedMovie(movie: Movie[]) {
-    const sonic = movie.find((movie: Movie) =>
-      movie.title.toLowerCase().includes("sonic")
+    const randomMovie: number = Number(
+      (Math.random() * movie.length).toFixed()
     );
-    if (!sonic) return setFeatured(movie.shift());
-    setFeatured(sonic);
+    setFeatured(movie[randomMovie]);
   }
 
   async function getAction() {
@@ -65,49 +52,12 @@ const Home: React.FC = () => {
   async function getComedy() {
     const data = await fetchMoviesWithGenre(35, "12,28,16");
     setComedy(data.results);
+    setLoading(false);
   }
 
   async function getCrime() {
     const data = await fetchMoviesWithGenre(80, "12,28,16,35");
     setCrime(data.results);
-  }
-
-  async function getDocu() {
-    const data = await fetchMoviesWithGenre(99, "12,28,16,35,80");
-    setDocu(data.results);
-  }
-
-  async function getDrama() {
-    const data = await fetchMoviesWithGenre(18, "12,28,16,35,80,99");
-    setDrama(data.results);
-  }
-
-  async function getFamily() {
-    const data = await fetchMoviesWithGenre(10751, "12,28,16,35,80,99,18");
-    setFamily(data.results);
-  }
-
-  async function getFantasy() {
-    const data = await fetchMoviesWithGenre(14, "12,28,16,35,80,99,18,10751");
-    setFantasy(data.results);
-  }
-
-  async function getHistory() {
-    const data = await fetchMoviesWithGenre(
-      36,
-      "12,28,16,35,80,99,18,10751,14"
-    );
-    setHistory(data.results);
-    setLoading(false);
-  }
-
-  async function getHorror() {
-    const data = await fetchMoviesWithGenre(
-      27,
-      "12,28,16,35,80,99,18,10751,14,36"
-    );
-    setHorror(data.results);
-    setLoading(false);
   }
 
   return (
@@ -136,12 +86,6 @@ const Home: React.FC = () => {
           <GenrePreviewContainer data={animation} title="Animation" id={16} />
           <GenrePreviewContainer data={comedy} title="Comedy" id={35} />
           <GenrePreviewContainer data={crime} title="Crime" id={80} />
-          <GenrePreviewContainer data={docu} title="Documentary" id={99} />
-          <GenrePreviewContainer data={drama} title="Drama" id={18} />
-          <GenrePreviewContainer data={family} title="Family" id={10751} />
-          <GenrePreviewContainer data={fantasy} title="Fantasy" id={14} />
-          <GenrePreviewContainer data={history} title="History" id={36} />
-          <GenrePreviewContainer data={horror} title="Horror" id={27} />
         </Section>
       )}
     </>
@@ -149,9 +93,3 @@ const Home: React.FC = () => {
 };
 
 export default Home;
-
-/* const randomMovie: number = Number(
-      (Math.random() * movie.length).toFixed()
-    );
-    const selectMovie = movie[randomMovie]; */
-// setFeatured(selectMovie);
